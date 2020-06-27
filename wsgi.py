9788fccc26f3057dd8db8e65c63ff60e38f77855
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-06-27 13:29:04 -0400

    fix path for real

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,6 @@ activate_this = "/opt/wlanpi/wlanpi-webui/venv/bin/activate_this.py"
 with open(activate_this) as _file:
     exec(_file.read(), dict(__file__=activate_this))
 
-from wlanpi-webui import create_app
+from wlanpi_webui import create_app
 
 application = create_app()
