ba49bb9db1404884c651affa3c7a450d6114f95e
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-06-27 20:35:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,105 +11,98 @@ echo "start..."
 
 # MAIN APP
 
-REPO="https://github.com/joshschmelzle/wlanpi-webui.git"
-REPONAME="wlanpi-webui"
-WLANPIDIR="/opt/wlanpi"
-USER="wlanpi"
+webui_repo="https://github.com/joshschmelzle/wlanpi-webui.git"
+webui_repo_name="wlanpi-webui"
+app_dir="/opt/wlanpi"
+user="wlanpi"
 
-if [[ -d "$WLANPIDIR" ]]; then
-    echo "$WLANPIDIR already exists... skipping..."
+if [[ -d "$app_dir" ]]; then
+    echo "$app_dir already exists... skipping..."
 else
-    echo "creating $WLANPIDIR and changing owner to $USER..." 
-    sudo mkdir $WLANPIDIR
+    echo "creating $app_dir and changing owner to $USER..." 
+    sudo mkdir $app_dir
 fi
 
-if [[ -d "$WLANPIDIR/$REPONAME" ]]; then
-    echo "$WLANPIDIR/$REPONAME already exists... skipping..."
+if [[ -d "$app_dir/$webui_repo_name" ]]; then
+    echo "$app_dir/$webui_repo_name already exists... skipping..."
 else
-    git clone $REPO $WLANPIDIR/$REPONAME
+    git clone $webui_repo $app_dir/$webui_repo_name
 fi
 
 activate () {
-    . $WLANPIDIR/$REPONAME/venv/bin/activate
+    . $app_dir/$webui_repo_name/venv/bin/activate
 }
 
-if [[ -d "$WLANPIDIR/$REPONAME/venv" ]]; then
-    echo "$WLANPIDIR/$REPONAME/venv already exists... skipping..."
+if [[ -d "$app_dir/$webui_repo_name/venv" ]]; then
+    echo "$app_dir/$webui_repo_name/venv already exists... skipping..."
 else
-    sudo chown -R $USER:$USER $WLANPIDIR
-    sudo -u $USER python3 -m virtualenv -p=python3 $WLANPIDIR/$REPONAME/venv
+    sudo chown -R $user:$user $app_dir
+    sudo -u $user python3 -m virtualenv -p=python3 $app_dir/$webui_repo_name/venv
     activate
-    pip install -r $WLANPIDIR/$REPONAME/requirements.txt
-    pip install $WLANPIDIR/$REPONAME/.
+    pip install -r $app_dir/$webui_repo_name/requirements.txt
+    pip install $app_dir/$webui_repo_name/.
     deactivate
 fi
 
 # SPEEDTEST APP
 
-SPEEDTESTDIR="/var/www/wlanpi-speedtest"
-WWW="www-data" 
+speedtest_dir="/var/www/wlanpi-speedtest"
+www_user="www-data" 
  
-if [[ -d "$SPEEDTESTDIR" ]]; then
-    echo "$SPEEDTESTDIR already exists... skipping..."
+if [[ -d "$speedtest_dir" ]]; then
+    echo "$speedtest_dir already exists... skipping..."
 else
-    echo "creating $SPEEDTESTDIR..."
-    sudo mkdir $SPEEDTESTDIR
-    echo "copying files to $SPEEDTESTDIR..."
-    cp $WLANPIDIR/$REPONAME/speedtest/src/* $SPEEDTESTDIR -r
-    echo "changing $SPEEDTESTDIR owner to $WWW"
-    sudo chown -R $WWW:$WWW $SPEEDTESTDIR
-fi
-
-
-if [[ -d "$WLANPIDIR/$REPONAME" ]]; then
-    echo "$WLANPIDIR/$REPONAME already exists... skipping..."
-else
-    git clone $REPO $WLANPIDIR/$REPONAME
+    echo "creating $speedtest_dir..."
+    sudo mkdir $speedtest_dir
+    echo "copying files to $speedtest_dir..."
+    cp $app_dir/$webui_repo_name/speedtest/src/* $speedtest_dir -r
+    echo "changing $speedtest_dir owner to $www_user"
+    sudo chown -R $www_user:$www_user $speedtest_dir
 fi
 
 # PHP
 
-PHP_PKG="php"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $PHP_PKG | grep "install ok installed")
-echo "checking for $PHP_PKG: $PKG_OK"
-if [[ "" = "$PKG_OK" ]]; then
-    echo "$PHP_PKG not installed. Setting up $PHP_PKG..."
+php_pkg="php"
+pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $php_pkg | grep "install ok installed")
+echo "checking for $php_pkg: $pkg_ok"
+if [[ "" = "$pkg_ok" ]]; then
+    echo "$pkg_ok not installed. Setting up $php_pkg..."
     apt --yes install php
 fi
 
 # APACHE2 
 
-MOD_WSGI_PKG="libapache2-mod-wsgi-py3"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $MOD_WSGI_PKG | grep "install ok installed")
-echo "checking for $MOD_WSGI_PKG: $PKG_OK"
+mod_wsgi_pkg="libapache2-mod-wsgi-py3"
+pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $mod_wsgi_pkg | grep "install ok installed")
+echo "checking for $mod_wsgi_pkg: $pkg_ok"
 if [[ "" = "$PKG_OK" ]]; then
-    echo "$MOD_WSGI_PKG not installed. Setting up $MOD_WSGI_PKG..."
+    echo "$mod_wsgi_pkg not installed. Setting up $mod_wsgi_pkg..."
     apt --yes install libapache2-mod-wsgi-py3
     sudo a2enmod wsgi
 fi
 
-APACHE_CONF="wlanpi-webui.conf"
-if [[ ! -f /etc/apache2/sites-available/$APACHE_CONF ]]; then
-    cp $WLANPIDIR/$REPONAME/$APACHE_CONF /etc/apache2/sites-available/$APACHE_CONF
+webui_app_conf="wlanpi-webui.conf"
+if [[ ! -f /etc/apache2/sites-available/$webui_app_conf ]]; then
+    cp $app_dir/$webui_repo_name/$webui_app_conf /etc/apache2/sites-available/$webui_app_conf
 fi
 
-SPEED_CONF="wlanpi-speedtest.conf"
-if [[ ! -f /etc/apache2/sites-available/$SPEED_CONF ]]; then
-    cp $WLANPIDIR/$REPONAME/$SPEED_CONF /etc/apache2/sites-available/$SPEED_CONF
+webui_speedtest_conf="wlanpi-speedtest.conf"
+if [[ ! -f /etc/apache2/sites-available/$webui_speedtest_conf ]]; then
+    cp $app_dir/$webui_repo_name/$webui_speedtest_conf /etc/apache2/sites-available/$webui_speedtest_conf
 fi
 
 # APACHE2 NEEDS TO LISTEN ON 8080!
 
-PCONF=`grep -r "Listen 8080" /etc/apache2/ports.conf`
-if [[ "" = "$PCONF" ]]; then
+ports=`grep -r "Listen 8080" /etc/apache2/ports.conf`
+if [[ "" = "$ports" ]]; then
     echo "Adding 8080 to /etc/apache2/ports.conf"
     echo 'Listen 8080' >> /etc/apache2/ports.conf
 fi
 
 # UFW NEEDS TO ALLOW 8080
 
-UFW=`ufw status numbered | grep 8080`
-if [[ "" = "$UFW" ]]; then
+ufw=`ufw status numbered | grep 8080`
+if [[ "" = "$ufw" ]]; then
     echo "Punching a hole in the firewall (ง •̀_•́)ง..."
     ufw allow 8080
 fi 
@@ -122,8 +115,9 @@ a2ensite wlanpi-speedtest.conf
 
 # PERMISSIONS!
 
-echo "making sure $WLANPIDIR owner is $USER..."
-sudo chown -R $USER:$USER $WLANPIDIR
+echo "making sure $WLANPIDIR owner is $user..."
+sudo chown -R $user:$user $app_dir
+sudo chmod +s /usr/bin/arping
 
 # REBOOT APACHE
 
