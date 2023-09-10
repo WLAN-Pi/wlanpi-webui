32f13b3582fcbb43759a08520ecd5521db4fb48a
Josh Schmelzle <josh@joshschmelzle.com>
2023-09-09 20:24:57 -0400

    fix proxy

--- dev.sh
+++ dev.sh
@@ -1,3 +1,6 @@
 unlink /etc/nginx/sites-enabled/wlanpi_webui.conf
 ln -s /home/wlanpi/wlanpi-webui/install/etc/nginx/wlanpi_webui.conf /etc/nginx/sites-enabled/wlanpi_webui.conf
+unlink /etc/nginx/sites-enabled/wlanpi_librespeed.conf
+ln -s /home/wlanpi/wlanpi-webui/install/etc/nginx/wlanpi_librespeed.conf /etc/nginx/sites-enabled/wlanpi_librespeed.conf
 systemctl restart wlanpi-webui
+systemctl restart nginx
