32c0328758d9e09f2c4bf1b3f390fddbd416311f
Josh Schmelzle <josh@joshschmelzle.com>
2023-09-12 11:08:31 -0400

    adds to system and alert when connection is down

--- wlanpi_webui/about/about.py
+++ wlanpi_webui/about/about.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from flask import render_template, request
 
 from wlanpi_webui.about import bp
@@ -7,12 +9,29 @@ from wlanpi_webui.utils import is_htmx, system_service_running_state
 
 @bp.route("/about")
 def about():
+    try:
+        kernel_version = subprocess.check_output("uname -r", shell=True).decode()
+    except Exception:
+        kernel_version = "unknown"
+    try:
+        hardware_model = subprocess.check_output(
+            "cat /proc/cpuinfo | grep Model | cut -d ':' -f 2", shell=True
+        ).decode()
+    except Exception:
+        hardware_model = "unknown"
+    try:
+        mode = subprocess.check_output("cat /etc/wlanpi-state", shell=True).decode()
+    except Exception:
+        mode = "unknown"
     resp_data = {
+        "mode": mode,
         "hostname": get_hostname(),
         "wlanpi_core_version": get_apt_package_version("wlanpi-core"),
         "wlanpi_core_status": (
             "active" if system_service_running_state("wlanpi-core") else "inactive"
         ),
+        "kernel_version": kernel_version,
+        "hardware_model": hardware_model,
         "wlanpi_version": Config.WLANPI_VERSION,
         "webui_version": Config.WEBUI_VERSION,
     }
