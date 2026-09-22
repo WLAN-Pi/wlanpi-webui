b59459ed63aa92910266ed2bc20a71ef95c4ad4a
Josh Schmelzle <josh@joshschmelzle.com>
2026-09-22 02:30:57 -0400

    ci(lint): ignore deployment/legacy-* in shellcheck; add dev.sh shebang
    The shellcheck job added in #129 fails on the historical deployment/legacy-*
    scripts (not shipped) and on dev.sh, which had no shebang. Exclude legacy and
    give dev.sh a shebang.

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 unlink /etc/nginx/sites-enabled/wlanpi_webui.conf
 ln -s /home/wlanpi/wlanpi-webui/install/etc/nginx/wlanpi_webui.conf /etc/nginx/sites-enabled/wlanpi_webui.conf
 unlink /etc/nginx/sites-enabled/wlanpi_librespeed.conf
